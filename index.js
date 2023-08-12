53e9a34f32b87c150d1140b737337c444cc2d377
abdu95873 <abdu95873@gmail.com>
2023-08-12 17:44:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = "mongodb+srv://royalExpress:B8QtXz6EKSKdG4tk@cluster0.mh16alw.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const areaCollection = client.db("royalExpress").collection("areaName");
+    const usersCollection = client.db("royalExpress").collection("users");
+    const ordersCollection = client.db("royalExpress").collection("orders");
+
+
+
+    //************************************************************users collection***************************************************
+
+    app.get('/users',async (req, res)=>{
+      let query = {};
+      if(req.query.email){
+          query = {
+              email: req.query.email
+          }
+          
+      }
+      
+
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+  });
+
+
+    app.post('/users', async(req, res)=>{
+        const user = req.body;
+        console.log(user);
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+
+    });
+
+
+
+   //..........................................................area collection ......................................................
+
+
+   app.get('/areas', async(req, res)=>{
+    const cursor = areaCollection.find()
+    const result = await cursor.toArray();
+    res.send(result);
+    
+   })
+
+
+   //..................order..............
+
+   app.post('/orders', async(req, res)=>{
+    const order = req.body;
+    console.log(order);
+
+    const result = await ordersCollection.insertOne(order);
+    res.send(result);
+   });
+
+
+   app.get('/orders', async(req, res)=>{
+    let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const orders = await ordersCollection.find(query).toArray();
+            res.send(orders);
+    
+   })
+
+
+   app.put('/manage-orders', async(req, res)=> {
+    const body = req.body;
+    const options = {upsert: true};
+    const id = body.id;
+    const filter = {_id: new ObjectId(id)};
+    const updateDoc ={
+        $set:{
+            status: body.status,
+            
+            
+        }
+    }
+    console.log(updateDoc);
+    const result = await ordersCollection.updateOne(filter, updateDoc, options);
+    res.send(result);
+
+});
+
+
+
+
+
+// .........................................
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) =>{
+   res.send('Simple Crud Is Running'); 
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on PORT: ${port}`)
+});
